02362de49527d9865849d495766bf52d09ddf98b
HamidKhan1001 <hamidk5002@gmail.com>
2025-06-10 13:03:22 +0500

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const checkAuth = (req, res, next) => {
-    if (req.session && req.session.userId) {
-        next();
-    } else {
-        res.status(401).json({ message: 'Please login first' });
-    }
-};
-
-module.exports = checkAuth;
